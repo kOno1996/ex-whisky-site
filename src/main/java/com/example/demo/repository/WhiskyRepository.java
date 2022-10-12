bdc41feda2bba7eaefac94aa1932b2545dde33d4
ono <keitaro.ono@rakus-partners.co.jp>
2022-10-13 00:55:45 +0900

    スクレイピング

--- src/main/java/com/example/demo/repository/WhiskyRepository.java
+++ src/main/java/com/example/demo/repository/WhiskyRepository.java
@@ -1,12 +1,18 @@
 package com.example.demo.repository;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -15,29 +21,47 @@ import com.example.demo.domain.Whisky;
 
 @Repository
 public class WhiskyRepository {
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate template;
-	
-	private static final RowMapper<Whisky> WHISKY_ROW_MAPPER = (rs, i)->{
+
+	private static final RowMapper<Whisky> WHISKY_ROW_MAPPER = (rs, i) -> {
 		Whisky whisky = new Whisky();
 		whisky.setId(rs.getInt("id"));
-		//whisky.setName(rs.getString("name"));
+		// whisky.setName(rs.getString("name"));
 		whisky.setBrand(rs.getString("brand"));
-		//whisky.setProductingArea(rs.getString("producting_area"));
-		//whisky.setClassification(rs.getString("classification"));
+		// whisky.setProductingArea(rs.getString("producting_area"));
+		// whisky.setClassification(rs.getString("classification"));
 		whisky.setPrice(rs.getInt("price"));
-		//whisky.setAlcoholContent(rs.getInt("alcohol_content"));
-		//whisky.setManufacturer(rs.getString("manufacturer"));
+		// whisky.setAlcoholContent(rs.getInt("alcohol_content"));
+		// whisky.setManufacturer(rs.getString("manufacturer"));
 		whisky.setMaturityYears(rs.getInt("maturity_years"));
-		//whisky.setCapacity(rs.getInt("capacity"));
+		// whisky.setCapacity(rs.getInt("capacity"));
 		return whisky;
 	};
-	
-	public List<Whisky> whiskyList() throws IOException{
+
+	public List<Whisky> whiskyList() throws IOException {
 		StringBuilder sql = new StringBuilder();
-		Document doc = Jsoup.parse(new File("/jsoup.html"), "UTF-8");
+
+		// HTMLファイルを読み込む
+		InputStream is = new ClassPathResource("static/css/jsoup.html").getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		
+	            String line;
+
+	            // 1行ずつファイルを読み込む
+	            while ((line = br.readLine()) != null) {
+	                System.out.println(line);
+	            }
+		Document doc = Jsoup.parse(br.toString(), "UTF-8");
 		System.out.println("\n\n\n\n\n\n\n\n" + doc.html());
+
+		//aタグ要素のテキストを取り出す
+		Elements elements = doc.select("a");
+		for (Element element : elements) {
+			System.out.println(element.text());
+		}
+
 		sql.append("SELECT id, brand, price, maturity_years FROM whisky");
 		return template.query(sql.toString(), WHISKY_ROW_MAPPER);
 	}
